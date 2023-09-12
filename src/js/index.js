d5f010c59f632bdd859e418ec885c8d41105ba23
John GC <gc.john.dcg@gmail.com>
2023-09-11 23:52:18 -0600

    feat-> tooltip translate ready

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const apiURL = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json";
 
     // ! COLOR SHEMA ! //
-    const colorScheme = ["#a50026", "#d73027", "#f46d43", "#fdae61", "#fee090", "#ffffbf", "#e0f3f8", "#abd9e9", "#74add1", "#4575b4", "#313695"];
+    const colorScheme = ["#67001f", "#b2182b", "#d6604d", "#f4a582", "#fddbc7", "#f7f7f7", "#d1e5f0", "#92c5de", "#4393c3", "#2166ac", "#053061"];
 
     var legendColors = colorScheme.reverse();
     // ! CREATE GENERAL CONTAINER ! //
@@ -166,10 +166,14 @@ document.addEventListener("DOMContentLoaded", () => {
             .attr("height", (d) => yScale.bandwidth(d.month))
             .attr("fill", (d) => legendThreshold(baseTemperature + d.variance))
             .on("mouseover", (event, d) => {
-                tooltip.style("opacity", 1).attr("data-year", d.year);
-                document.querySelector(".date_tip").textContent = new Date(d.year, d.month);
-                document.querySelector(".temp_tip").textContent = baseTemperature + d.variance;
-                document.querySelector(".var_tip").textContent = d.variance;
+                var newDate = new Date(d.year, d.month);
+                tooltip
+                    .style("opacity", 1)
+                    .style("translate", `${xScale(d.year)}px ${yScale(d.month)}px`)
+                    .attr("data-year", d.year);
+                document.querySelector(".date_tip").textContent = d3.utcFormat("%Y - %B")(newDate);
+                document.querySelector(".temp_tip").textContent = d3.format(".1f")(baseTemperature + d.variance);
+                document.querySelector(".var_tip").textContent = d3.format("+.1f")(d.variance);
             })
             .on("mouseout", () => {
                 tooltip.style("opacity", 0);
